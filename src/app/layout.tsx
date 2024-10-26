2739f802bfc358b0f2a3e7200ffc1eac7cad7e5b
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-10-26 21:36:32 +0000

    feat: added wix context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { WixclientContextProvider } from "@/context/WixContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <WixclientContextProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </WixclientContextProvider>
       </body>
     </html>
   );
---
+++ src/context/WixContext.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { createClient, OAuthStrategy } from "@wix/sdk";
+import { products, collections } from "@wix/stores";
+import Cookies from "js-cookie";
+import { createContext, ReactNode } from "react";
+
+const refreshToken = JSON.parse(Cookies.get("refreshToken") || "{}");
+
+const myWixClient = createClient({
+  modules: {
+    products,
+    collections,
+  },
+  auth: OAuthStrategy({
+    clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+    tokens: {
+      refreshToken,
+      accessToken: { value: "", expiresAt: 0 },
+    },
+  }),
+});
+
+export type wixClientType = typeof myWixClient;
+
+export const WixClientContext = createContext<wixClientType>(myWixClient);
+
+export const WixclientContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  return (
+    <WixClientContext.Provider value={myWixClient}>
+      {children}
+    </WixClientContext.Provider>
+  );
+};
